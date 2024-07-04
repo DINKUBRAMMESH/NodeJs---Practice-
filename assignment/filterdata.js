6d9469929aa76c9dc17ec668a8124dba86da2df3
DINKUBRAMMESH <dinkubrammesh07@gmail.com>
2024-07-04 09:31:28 +0530

    learning node

--- assignment/filterdata.js
+++ assignment/filterdata.js
@@ -1,19 +1,39 @@
+// const fs = require('fs');
+
+// fs.readFile('users.json', 'utf-8', (err, data) => {
+//     if (err) throw err;
+    
+//     let users = JSON.parse(data);
+//     let maleUsers = [];
+//     let femaleUsers = [];
+    
+//     for (let user of users) {
+//         if (user.gender === 'Male') {
+//             maleUsers.push(user);
+//         } else if (user.gender==='Female'){
+//             femaleUsers.push(user);
+//         }
+//     }
+
+//     fs.writeFile('male.json', JSON.stringify(maleUsers), err => {
+//         if (err) throw err;
+//     });
+
+//     fs.writeFile('female.json', JSON.stringify(femaleUsers), err => {
+//         if (err) throw err;
+//     });
+// });   
+
+
 const fs = require('fs');
 
 fs.readFile('users.json', 'utf-8', (err, data) => {
     if (err) throw err;
     
     let users = JSON.parse(data);
-    let maleUsers = [];
-    let femaleUsers = [];
-    
-    for (let user of users) {
-        if (user.gender === 'Male') {
-            maleUsers.push(user);
-        } else if (user.gender==='Female'){
-            femaleUsers.push(user);
-        }
-    }
+
+    let maleUsers = users.filter(user => user.gender === 'Male');
+    let femaleUsers = users.filter(user => user.gender === 'Female');
 
     fs.writeFile('male.json', JSON.stringify(maleUsers), err => {
         if (err) throw err;
