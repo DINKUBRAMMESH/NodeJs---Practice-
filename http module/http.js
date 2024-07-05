8258de57f085e68ffc671b048c2e7b2d7b3c7999
DINKUBRAMMESH <dinkubrammesh07@gmail.com>
2024-07-05 09:19:07 +0530

    learning http

---
+++ http module/http.js
@@ -0,0 +1,9 @@
+const http =require('http')
+let server = http.createServer((req,res)=>{
+    res.end("hello ,gm")
+})
+
+server.listen(8080,'127.0.0.1',(err)=>{
+    if(err) throw err
+    console.log("server running")
+})
